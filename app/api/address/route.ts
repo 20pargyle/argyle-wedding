fdac5a0b3fb1213a12641df5555ddec5715a2517
Carter Singletary <120229354+cartersingle@users.noreply.github.com>
2023-10-07 11:24:53 -0600

    Update route.ts

--- app/api/address/route.ts
+++ app/api/address/route.ts
@@ -7,7 +7,7 @@ export async function POST(req: Request) {
   function authorize() {
     const auth = new google.auth.GoogleAuth({
       credentials: {
-        client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+        client_email: "website@wedding-storage.iam.gserviceaccount.com",
         client_id: process.env.CLIENT_ID,
         private_key: (process.env.GOOGLE_SHEETS_PRIVATE_KEY || "").replace(
           /\\n/g,
