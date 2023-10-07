cdf07c44f42246b573673bd42bc7d60b46203134
Carter Singletary <120229354+cartersingle@users.noreply.github.com>
2023-10-07 15:06:48 -0600

    Update favicon and optimize

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
-import ToastProvider from "@/components/toast-provider";
 import "./globals.css";
+
 import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 
+import ToastProvider from "@/components/toast-provider";
+
 export const metadata: Metadata = {
   title: "Meg and Carter",
-  description: "Wedding Website",
+  description: "A Wedding Website",
 };
 
 const lato = Lato({
   subsets: ["latin"],
-  weight: ["100", "300", "400", "700"],
+  weight: ["300", "400"],
 });
 
 export default function RootLayout({
