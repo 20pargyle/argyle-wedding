215414de2d5a80815cd97bd832195c6f4f452772
Carter Singletary <120229354+cartersingle@users.noreply.github.com>
2023-10-07 11:20:10 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    GOOGLE_SHEETS_CLIENT_EMAIL: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    GOOGLE_SHEETS_PRIVATE_KEY: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+    SHEET_ID: process.env.SHEET_ID,
+    SHEET_RANGE: process.env.SHEET_RANGE,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
