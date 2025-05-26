91d20309b6b0f681741de31f43c266452a5ea761
20pargyle <parkerargyle02@gmail.com>
2025-05-26 00:29:17 -0600

    album finalish

--- app/album/page.tsx
+++ app/album/page.tsx
@@ -12,36 +12,38 @@ export default async function Page() {
   return (
     <>
       <main>
-        <section className="flex flex-wrap gap-6 w-full">
+        <h1 className="m-8 text-center text-5xl">Pictures!</h1>
+        <section className="flex flex-wrap justify-center w-full">
           <Image
             src={img1}
             alt="Parker and Hannah"
-            className="max-w-xl"
+            className="max-w-xl m-6"
           />
           <Image
             src={img2}
             alt="Parker and Hannah"
-            className="max-w-xl"
-          />
-          <Image
-            src={img3}
-            alt="Parker and Hannah"
-            className="max-w-xl"
+            className="max-w-xl m-6"
+            priority
           />
           <Image
             src={img4}
             alt="Parker and Hannah"
-            className="max-w-xl"
+            className="max-w-xl m-6"
+          />
+          <Image
+            src={img3}
+            alt="Parker and Hannah"
+            className="max-w-xl m-6"
           />
           <Image
             src={img5}
             alt="Parker and Hannah"
-            className="max-w-xl"
+            className="max-w-xl m-6"
           />
           <Image
             src={img6}
             alt="Parker and Hannah"
-            className="max-w-xl"
+            className="max-w-xl m-6"
           />
         </section>
       </main>
